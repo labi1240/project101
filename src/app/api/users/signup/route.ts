6c60fe6aab55c1554cb424115983e8d80ee16748
labi1240 <labi1240@gmail.com>
2024-04-01 02:03:46 -0400

    feat: Updated src/app/api/users/signup/route.ts

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,31 +1,32 @@
 import { connect } from "@/database/dbConfig/dbConfig"; // Adjusted if dbConfig is indeed in the database folder
-import User from "@/database/models/userModel"; // Adjusted to use the absolute path alias
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { sendEmail } from "@/database/helpers/mailer";
-
-
+import jwt from "jsonwebtoken";
 
 connect()
 
 
 export async function POST(request: NextRequest) {
               try {
-                            const reqBody = await request.json()
-                            const { username, email, password } = reqBody
 
-                            console.log(reqBody);
+        const reqBody = await request.json()
+        const {email, password} = reqBody;
+        console.log(reqBody);
 
-                            //check if user already exists
-                            const user = await User.findOne({ email })
+        //check if user exists
+        const user = await User.findOne({email})
+        if(!user){
+            return NextResponse.json({error: "User does not exist"}, {status: 400})
+        }
+        console.log("user exists");
 
-                            if (user) {
-                                          return NextResponse.json({ error: "User already exists" }, { status: 400 })
-                            }
-
-                            //hash password
-                            const salt = await bcryptjs.genSalt(10)
-                            const hashedPassword = await bcryptjs.hash(password, salt)
+        //check if password is correct
+        const validPassword = await bcryptjs.compare(password, user.password)
+        if(!validPassword){
+            return NextResponse.json({error: "Invalid password"}, {status: 400})
+        }
+        console.log(user);
+        //create token data
 
                             const newUser = new User({
                                           username,
@@ -33,24 +34,25 @@ export async function POST(request: NextRequest) {
                                           password: hashedPassword
                             })
 
-                            const savedUser = await newUser.save()
-                            console.log(savedUser);
+        //create token data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+        //create token
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: "1d"})
 
-                            //send verification email
+        const response = NextResponse.json({
+            message: "Login successful",
+            success: true,
+        })
+        response.cookies.set("token", token, {
+            httpOnly: true,
+        })
+        return response;
 
-                            await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id })
-
-                            return NextResponse.json({
-                                          message: "User created successfully",
-                                          success: true,
-                                          savedUser
-                            })
-
-
-
-
-              } catch (error: any) {
-                            return NextResponse.json({ error: error.message }, { status: 500 })
-
-              }
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
 }
